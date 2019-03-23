6c075b0d0a2624eedf266e172e112f4154f0a37d
rafactor <avila.rafael@gmail.com>
2019-03-23 13:13:14 -0400

    production test

---
+++ client/build/precache-manifest.7d6f9ae2c7724bffcda073c554a0b28e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6cd8dea2015a496a6be7",
+    "url": "/static/js/main.691a8e91.chunk.js"
+  },
+  {
+    "revision": "b9dea1b1e2ee7fb08b42",
+    "url": "/static/js/2.ab833185.chunk.js"
+  },
+  {
+    "revision": "6cd8dea2015a496a6be7",
+    "url": "/static/css/main.c6ea029f.chunk.css"
+  },
+  {
+    "revision": "1e7bd8918683a05cf2b5afcf064b1faa",
+    "url": "/index.html"
+  }
+];
